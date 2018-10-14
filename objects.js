31bcda9879649ee88101b73ab38aa8b33a54d98b
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 19:58:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistNames: 'songTitles',
   }
 
-function updatePlaylist(playlist,artistName,songTitle) {
-  playlist.artistName = 'songTitle'
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
   return playlist
 }
